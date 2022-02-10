51f26ddb9cd8b60ae27562e28f3b7a3b282c64b9
rybadour <rybadour@gmail.com>
2022-02-10 15:35:06 -0500

    Saving work
     - Basic version of item list command and needed items command

---
+++ MyCommands.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Eco.Gameplay.Players;
+using Eco.Gameplay.Systems.Chat;
+using Eco.Gameplay.Property;
+using Eco.Gameplay.Objects;
+using Eco.Gameplay.Components;
+using Eco.Gameplay.Items;
+using Eco.EM.Framework.ChatBase;
+
+namespace EcoRebalanced
+{
+    public class MyCommands : IChatCommandHandler
+    {
+        [ChatCommand("List all items in your storage. Can filter by tag with 'tagFilter'.", "my-items", ChatAuthorizationLevel.User)]
+        public static void MyItems(User user, string tagFilter = "ALL")
+        {
+            if (tagFilter == null) tagFilter = "ALL";
+
+            SortedDictionary<string, int> itemTotals = Utils.getAllUserItems(user, tagFilter);
+
+            string listContents = "";
+            foreach (var item in itemTotals)
+            {
+                listContents += $"{item.Key} - {item.Value}\n";
+            }
+            ChatBaseExtended.CBInfoPane(
+                "List of Your Items" + (tagFilter == "ALL" ? "" : " (" + tagFilter + ")"),
+                listContents,
+                "list-items"
+            );
+        }
+
+        [ChatCommand("List items needed by current projects", "my-needed-items", ChatAuthorizationLevel.User)]
+        public static void MyNeededItems(User user)
+        {
+            string listContents = "";
+            SortedDictionary<string, int> itemsRemaining = Utils.getAllUserItems(user, "ALL");
+
+            foreach (CraftingComponent table in Utils.getOwnedComponents<CraftingComponent>(user))
+            {
+                foreach (WorkOrder order in table.WorkOrders.AsEnumerable())
+                {
+                    foreach (IStack stack in order.IngredientsRemaningInAllIterations())
+                    {
+                        int amountNeeded = stack.Quantity;
+                        Tag tag = TagManager.Tag(stack.StackObject.Name);
+                        if (tag != null)
+                        {
+                            // Iterate over items in tag and subtract from items in order until amountNeeded is exhausted
+                            // or all items were iterated through.
+                            foreach (Item item in TagExtensions.TaggedItems(tag))
+                            {
+                                amountNeeded = Utils.subtractFromItem(itemsRemaining, item.DisplayName, amountNeeded, true);
+                                if (amountNeeded == 0) break;
+                            }
+
+                            if (amountNeeded > 0)
+                            {
+                                Utils.subtractFromItem(itemsRemaining, tag.DisplayName, amountNeeded, false);
+                            }
+                        }
+                        else
+                        {
+                            Utils.subtractFromItem(itemsRemaining, stack.StackObject.DisplayName, amountNeeded, false);
+                        }
+                    }
+                }
+            }
+
+            foreach (var item in itemsRemaining)
+            {
+                if (item.Value < 0)
+                    listContents += $"<ecoicon item='{item.Key}'></ecoicon>{item.Key} - {item.Value * -1}\n";
+            }
+
+            ChatBaseExtended.CBInfoBox(
+                "Your Projects are Missing These Items\n" +
+                listContents
+            );
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Eco.Gameplay.Players;
+using Eco.Gameplay.Property;
+using Eco.Gameplay.Objects;
+using Eco.Gameplay.Components;
+using Eco.Gameplay.Items;
+
+namespace EcoRebalanced
+{
+    class Utils
+    {
+        public static IEnumerable<T> getOwnedComponents<T>(User user)
+        {
+            /* *
+            foreach (Deed deed in user.GetAllProperty())
+            {
+                foreach (WorldObject wo in deed.GetObjectsCreatedByUser(user))
+                {
+                    StorageComponent storage = wo.GetComponent<StorageComponent>();
+            /* */
+            return user.GetAllProperty().SelectMany(p =>
+                p.GetObjectsCreatedByUser(user)
+                    .Select(o => o.GetComponent<T>())
+                    .Where(c => c != null)
+            );
+        }
+
+        public static SortedDictionary<string, int> getAllUserItems(User user, string tagFilter)
+        {
+            SortedDictionary<string, int> itemTotals = new SortedDictionary<string, int>();
+            foreach (StorageComponent storage in getOwnedComponents<StorageComponent>(user))
+            {
+                foreach (ItemStack stack in storage.Inventory.NonEmptyStacks)
+                {
+                    if (tagFilter != "ALL" && !stack.Item.TagNames().Contains(tagFilter)) continue;
+
+                    int total;
+                    bool contains = itemTotals.TryGetValue(stack.DisplayName(), out total);
+                    if (!contains) total = 0;
+
+                    itemTotals[stack.DisplayName()] = total + stack.Quantity;
+                }
+            }
+
+            return itemTotals;
+        }
+
+        // Subtracts some amount of some item. If there was less items than wanted to be subtracted the remainder is returned.
+        // If stopAtZero is true then the item amount will not go into the negative.
+        public static int subtractFromItem(IDictionary<string, int> items, string itemName, int amountToSubtract, bool stopAtZero)
+        {
+            int itemAmount;
+            bool contains = items.TryGetValue(itemName, out itemAmount);
+            if (contains)
+            {
+                if (stopAtZero && itemAmount < amountToSubtract)
+                {
+                    items[itemName] = 0;
+                }
+                else
+                {
+                    items[itemName] = itemAmount - amountToSubtract;
+                }
+            }
+            else if (!stopAtZero)
+            {
+                items.Add(itemName, -amountToSubtract);
+                itemAmount = 0;
+            }
+
+            return (itemAmount > amountToSubtract ? 0 : amountToSubtract - itemAmount);
+        }
+    }
+}
